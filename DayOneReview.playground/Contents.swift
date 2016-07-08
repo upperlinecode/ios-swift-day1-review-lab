bd33629269985a012967511cd5b01b7150f24af7
carsonlevine <carsonglevine@me.com>
2016-07-08 15:18:52 -0400

    first commit

---
+++ DayOneReview.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Let's write a tweet about what you did last weekend. Store it in a variable called tweet.
+
+
+
+
+
+
+//Write a conditional statement that shortens the tweet to 140 characters if it is longer than 140 characters.
+
+
+
+
+
+
+
+//Capitalize all of the letters in tweet
+
+
+
+
+
+
+
+//Make another conditional statement that handles two possibilities: If the tweet ends with a question mark, change the last character of tweet to an exclamation point. If it ends with a period, add the string " This is a serious tweet." to the end of tweet.
+
+
+
+
+
+
+
