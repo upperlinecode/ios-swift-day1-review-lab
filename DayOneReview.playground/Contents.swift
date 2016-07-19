c3b1e7a15ef1e23e0f044126338aece89a13d239
aabato <angelica.bato@gmail.com>
2016-07-19 15:04:21 -0400

    Update questions and add Readme

--- DayOneReview.playground/Contents.swift
+++ DayOneReview.playground/Contents.swift
@@ -2,22 +2,50 @@
 
 import UIKit
 
-//Let's write a tweet about what you did last weekend. Store it in a variable called tweet.
+
+// Let's write a tweet about what you did last weekend. Store it in a variable called tweet.
 
 
 
 
 
 
-//Write a conditional statement that shortens the tweet to 140 characters if it is longer than 140 characters.
+// Write a conditional statement that prints "This is an OK tweet" if the tweet is less than or equal to 140 characters
 
 
 
 
 
 
+// Capitalize all of the letters in tweet
 
-//Capitalize all of the letters in tweet
+
+
+
+
+
+// Make another conditional statement that handles two possibilities: If the tweet ends with a question mark, print "So many questions.". If it ends with a period, print "This is a serious tweet."
+
+
+
+
+
+// Create a variable that contains your username and insert your username before your tweet. Hint: Try using insert(_:atIndex:)
+
+
+
+
+
+// Make your tweet sound super excited --- add 3 exclamation points to the end of it.
+
+
+
+
+
+/*------------------------- ADVANCED --------------------------*/
+
+
+// Check if the tweet is longer than 140 characters; if so, shorten it to 140 characters.
 
 
 
@@ -26,10 +54,3 @@ import UIKit
 
 
 //Make another conditional statement that handles two possibilities: If the tweet ends with a question mark, change the last character of tweet to an exclamation point. If it ends with a period, add the string " This is a serious tweet." to the end of tweet.
-
-
-
-
-
-
-
